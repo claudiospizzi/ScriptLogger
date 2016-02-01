21e2d8ff57c1a6c12b6cc174e1106f856a2f6d29
Claudio Spizzi <claudio.spizzi@live.com>
2016-02-01 13:00:27 +0100

    Update logger demo with new parameters

--- Examples/LoggerDemo.ps1
+++ Examples/LoggerDemo.ps1
@@ -2,8 +2,10 @@
 # Initialize the logger with default values
 Start-ScriptLogger
 
-# Alternative: Specify all possible parameters for the logger
-Start-ScriptLogger -Path 'C:\Temp\test.log' -Format '{0:yyyy-MM-dd}   {0:HH:mm:ss}   {1}   {2}   {3,-11}   {4}' -Level 'Verbose' -SkipEventLog -HideConsoleOutput
+# Alternative: Specify all possible parameters for the logger. Of course, this
+# will not log anything, if you disable the log file, the event log and the
+# console output at the same time.
+Start-ScriptLogger -Path 'C:\Temp\test.log' -Format '{0:yyyy-MM-dd}   {0:HH:mm:ss}   {1}   {2}   {3,-11}   {4}' -Level 'Verbose' -NoLogFile -NoEventLog -NoConsoleOutput
 
 # Log an error message
 Write-ErrorLog -Message 'My Error Message'
