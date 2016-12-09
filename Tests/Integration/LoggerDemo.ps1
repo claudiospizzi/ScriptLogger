50e6aeb191834ed6e6c4fce93b59b169e1f26d12
Claudio Spizzi <claudio.spizzi@live.com>
2016-12-09 16:07:23 +0100

    Update example

--- Tests/Integration/LoggerDemo.ps1
+++ Tests/Integration/LoggerDemo.ps1
@@ -3,13 +3,16 @@
 Start-ScriptLogger
 
 # Second options, specify multiple custom settings for the logger
-Start-ScriptLogger -Path 'C:\Temp\test.log' -Format '{0:yyyy-MM-dd}   {0:HH:mm:ss}   {1}   {2}   {3,-11}   {4}' -Level Warning -Encoding 'UTF7' -SkipEventLog -HideConsoleOutput
+Start-ScriptLogger -Path 'C:\Temp\test.log' -Format '{0:yyyy-MM-dd}   {0:HH:mm:ss}   {1}   {2}   {3,-11}   {4}' -Level Warning -Encoding 'UTF8' -NoEventLog -NoConsoleOutput
 
 # Get the current script logger configuration object
 Get-ScriptLogger
 
 # Update the script logger configuration
 Set-ScriptLogger -Level Verbose
+
+# Log an error record
+try { 0 / 0 } catch { Write-ErrorLog -ErrorRecord $_ }
 
 # Log an error message
 Write-ErrorLog -Message 'My Error Message'
