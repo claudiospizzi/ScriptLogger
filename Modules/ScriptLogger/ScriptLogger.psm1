024fbf2c4bc50fd3274d3852cc08f2b00f658ce5
Claudio Spizzi <claudio@spizzi.net>
2019-08-29 16:41:41 +0200

    Update build script

--- Modules/ScriptLogger/ScriptLogger.psm1
+++ Modules/ScriptLogger/ScriptLogger.psm1
@@ -7,8 +7,11 @@
         module context.
 #>
 
+#region Namepsace Loader
 
-## Module loader
+#endregion Namepsace Loader
+
+#region Module Loader
 
 # Get and dot source all classes (internal)
 Split-Path -Path $PSCommandPath |
@@ -28,11 +31,56 @@ Split-Path -Path $PSCommandPath |
         Get-ChildItem -Include '*.ps1' -File -Recurse |
             ForEach-Object { . $_.FullName }
 
+#endregion Module Loader
 
-## Module configuration
+#region Module Configuration
+
+#endregion Module Configuration
+
+
+
+
+<#
+    .SYNOPSIS
+        Root module file.
+
+    .DESCRIPTION
+        The root module file loads all classes, helpers and functions into the
+        module context.
+#>
+
+#region Namepsace Loader
+
+#endregion Namepsace Loader
+
+#region Module Loader
+
+# Get and dot source all classes (internal)
+Split-Path -Path $PSCommandPath |
+    Get-ChildItem -Filter 'Classes' -Directory |
+        Get-ChildItem -Include '*.ps1' -File -Recurse |
+            ForEach-Object { . $_.FullName }
+
+# Get and dot source all helper functions (internal)
+Split-Path -Path $PSCommandPath |
+    Get-ChildItem -Filter 'Helpers' -Directory |
+        Get-ChildItem -Include '*.ps1' -File -Recurse |
+            ForEach-Object { . $_.FullName }
+
+# Get and dot source all external functions (public)
+Split-Path -Path $PSCommandPath |
+    Get-ChildItem -Filter 'Functions' -Directory |
+        Get-ChildItem -Include '*.ps1' -File -Recurse |
+            ForEach-Object { . $_.FullName }
+
+#endregion Module Loader
+
+#region Module Configuration
 
 # Module path
 New-Variable -Name 'ModulePath' -Value $PSScriptRoot
 
 # Module wide array hosting all script loggers
 $Script:Loggers = @{}
+
+#endregion Module Configuration
--- build.settings.ps1
+++ build.settings.ps1
@@ -4,9 +4,9 @@ Properties {
     $ModuleNames    = 'ScriptLogger'
 
     $GalleryEnabled = $true
-    $GalleryKey     = Use-VaultSecureString -TargetName 'PowerShell Gallery Key'
+    $GalleryKey     = Use-VaultSecureString -TargetName 'PowerShell Gallery Key (claudiospizzi)'
 
     $GitHubEnabled  = $true
     $GitHubRepoName = 'claudiospizzi/ScriptLogger'
-    $GitHubToken    = Use-VaultSecureString -TargetName 'GitHub Token'
+    $GitHubToken    = Use-VaultSecureString -TargetName 'GitHub Token (claudiospizzi)'
 }
